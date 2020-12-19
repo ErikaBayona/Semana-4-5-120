4d25c7e736efd65c16503e7cf9d5fcdffbe8fa36
ErikaBayona <ilibayona@live.com>
2020-12-19 14:19:10 -0500

    front

---
+++ src/App.vue
@@ -0,0 +1,273 @@
+<template>
+<div>
+  <div v-if="estado">
+     <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark ">
+        <a class="navbar-brand " href="# ">
+            <img src="https://image.freepik.com/free-vector/established-international-trade-routes-selling-goods-overseas-export-control-export-controlled-materials-export-licensing-services-concept-pinkish-coral-bluevector-isolated-illustration_335657-1794.jpg" width="30 " height="30
+    " class="d-inline-block align-top " alt=" " loading="lazy "> Nombre de la empresa
+        </a>
+        <div class="collapse navbar-collapse justify-content-end " id="navbarNav ">
+            <ul class="navbar-nav ">
+                <li class="nav-item active ">
+                    <a class="nav-link " v-on:click="ingresar" href="# ">Sistema <span class="sr-only ">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div id="carrusel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1" class=""></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2" class=""></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://image.freepik.com/free-photo/industrial-port-container-yard_1112-1202.jpg" width="1410" height="500"/>
+                
+                
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>CARGA Y DESCARGA DE MERCANCIA</h5>
+                    
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://image.freepik.com/free-photo/transportation-logistics-container-cargo-ship-cargo-plane_37416-150.jpg" width="1410" height="500"/>
+                
+                
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>VENTA DE MAQUINARIA</h5>
+                    
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://image.freepik.com/free-photo/truck-highway-road-with-container-logistic-industrial-transport-with-sunrise-sky_42493-151.jpg" width="1410" height="500"/>
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>LOGISTICA DE TRANSPORTE DE MERCANCIA</h5>
+                    
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carrusel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <div>
+      <home/>
+    </div>
+    <footer class="page-footer bg-secondary text-white pt-4">
+
+        <!-- Footer Text -->
+        <div class="container-fluid text-center text-md-left">
+
+            <!-- Grid row -->
+            <div class="row">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">Información del sitio</h5>
+                    <p>Sitio creado para responder temporalmente a la actividad reto2 de
+                        Desarrollo de Software (Ciclo3)
+                        en el programa creado por MINTIC "Misión TIC 2022".</p>
+                    <h5 >
+                        Enlaces
+                    </h5>
+                    <p class="main-menu__link" style="margin-left: 5mm;">
+                        <a href="https://github.com/TitoFM16/Semana-2-120/" target="_blank">Reporsitorio GitHub</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mb-md-0 mb-3">
+
+                    <!-- Content -->
+                    <h5 class="text-uppercase font-weight-bold">Información de contactos</h5>
+                    <p><ul style="margin-right: 5mm;">
+                        <li>Eyder Alexander Herrera Carrillo: Desarrollador - Correo: <a href="alexxherrerac@gmail.com" target="_blank">alexxherrerac@gmail.com</a></li>
+                        <li>Aura Zuleta Gómez : Desarrollador - Correo: <a href="amzuletago@unal.edu.co" target="_blank">amzuletago@unal.edu.co</a></li>
+                        <li>Erika Bayona Cierra: Desarrollador - Correo: <a href="erika.bayona01@uptc.edu.co" target="_blank">erika.bayona01@uptc.edu.co</a></li>
+                        <li>Juan Francisco Figueroa Jimenez: Desarrollador - Correo: <a href="juffigueroama@unal.edu.co" target="_blank">juffigueroama@unal.edu.co</a></li>
+                        <li>Yoiler Mosquera Cordoba: Desarrollador - Correo: <a href="yoilerlp595@gmail.com" target="_blank">yoilerlp595@gmail.com</a></li>
+                    </ul></p>
+
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Footer Text -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-1 bg-dark">© 2020 Copyright: DAD
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+  </div>
+  <div v-else>
+  <v-app id="app">
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <template>
+          <v-list-item @click="home">
+            <v-list-item-action>
+              <v-icon>home</v-icon>
+            </v-list-item-action>
+            <v-list-item-title>
+              Inicio
+            </v-list-item-title>
+          </v-list-item>
+        </template>
+        <template v-if="logueado">
+          <v-list-group>
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                <v-list-item-title>
+                  Almacén
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item :to="{name: 'categoria'}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Categorías
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item :to="{name: 'articulo'}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Artículos
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-group>
+            <v-list-item slot="activator">
+              <v-list-item-content>
+                <v-list-item-title>
+                  Accesos
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item :to="{name: 'usuario'}">
+              <v-list-item-action>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  Usuarios
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </template> 
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="silver darken-3"
+      dark
+    >
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-3"
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="hidden-sm-and-down">Sistema</span>
+      </v-toolbar-title>      
+      <v-spacer></v-spacer>
+      <v-btn @click="salir()" icon v-if="logueado">
+        <v-icon>logout</v-icon> Salir
+      </v-btn>
+      <v-btn :to="{name: 'login'}" icon v-else>
+        <v-icon>apps</v-icon> Login
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container
+        fluid
+        fill-height
+      >
+        <v-slide-y-transition mode="out-in">
+          <router-view/>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+    <v-footer height="auto">
+      <v-layout justify-center>
+        <v-flex text-xs-center>
+          <v-card flat tile color="primary" class="white--text">
+            <v-card-text class="white--text pt-0">
+              Proyecto final &copy;2020
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+  </div>
+  </div>
+</template>
+
+<script>
+import home from './components/home.vue'
+export default {
+  components: { home },
+  name: 'App',
+
+    data () {
+      return {
+      drawer: false,
+      estado:1
+      }
+    },
+
+  computed:{
+    logueado(){
+      return this.$store.state.usuario;
+    },
+  },
+  created(){
+    this.$store.dispatch("autoLogin");
+  },
+  methods:{
+    salir(){
+      this.$store.dispatch("salir");
+      this.estado=1
+    },
+
+    ingresar(){
+      this.estado=0
+    },
+
+    home(){
+      this.estado=1
+
+    }
+  }
+};
+</script>
---
+++ src/components/home.vue
@@ -0,0 +1,101 @@
+<template>
+<div>
+    <div class="container-fluid">
+        <div class="row mt-md-5 mt-sm-5 mt-xs-5">
+            <div v-for="(art,indice) of datos" :key="art.id" class="col-lg-6 col-xs-12 border">
+                <div v-if="indice<=3">
+                <div  class="d-flex justify-content-center align-items-center">
+
+                    <div class="p-3">
+                        <svg width="12em" height="12em" viewBox="0 0 16 16" class="bi bi-alarm-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"
+                            />
+                        </svg>
+                    </div>
+
+                    <div class="p-2">
+                        <p>
+                            {{art.nombre}}
+                        </p>
+                    </div>
+
+                </div>
+                <div class="d-flex container-fluid justify-content-end pb-2 mt-n2">
+                    <button type="button" class="btn btn-outline-info">Info</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-center mb-5">
+            <div v-for="miembro in team" :key="miembro.codigo" class="col mt-5">
+                     <div class="card text-white bg-dark">
+                        <div class="d-flex justify-content-center p-2">
+                            <img :src="miembro.image">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Team member: {{miembro.codigo}} {{miembro.rol}}</h5>
+                            <h5 class="card-title"> {{miembro.nombre}}</h5>
+                            <p class="card-text">{{miembro.descripcion}}
+                            </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+
+<script>
+import axios from 'axios';
+export default {
+     data() {
+        return {
+            datos: null,
+            team: [{
+    codigo: 1,
+    nombre: "Juan Francisco Figueroa Martinez",
+    descripcion: "Universidad Nacional de Colombia - Sede Medellín. 21 años. Disfruto montar cicla",
+    rol: "Desarrollador backend",
+    image: "juan.jpeg"
+    },
+    {
+    codigo: 2,
+    nombre: "Erika Cecilia Bayona Sierra",
+    descripcion: "Universidad Pedagogica y Tecnologica de Colombia. 22 años. Disfruto ver series y peliculas.",
+    rol: "Product Owner",
+    image: "erika.jpeg"
+    },
+    {
+    codigo: 3,
+    nombre: "Yoyler Mosquera Cordoba",
+    descripcion: "Ingeniería de sistemas-Universidad Uniclaretiana. Desarrollador web, apasionado por informatica y la tecnologia.",
+    rol: "Desarrollador Frontend",
+    image: "yoyler.jpeg"
+    },
+    {
+    codigo: 4,
+    nombre: "Aura María Zuleta Gómez",
+    descripcion: "Ingeniera de Petróleos-UNAL, Magister en ing. Civil UFPE. 28 años. Me gusta viajar, leer y hacer Yoga",
+    rol: "Desarrollador Frontend",
+    image: "aura.jpeg"
+    },
+    {
+    codigo: 5,
+    nombre: "Eyder Alexander Herrerra",
+    descripcion: "Universidad Manuela Beltran. 22 años. Disfruto de montar cicla.",
+    rol: "Desarrollador backend",
+    image: "eyder.jpeg"
+    }]
+        };
+    },
+    mounted() {
+     axios.get('articulo/list')
+    .then(response => (this.datos = response.data));
+
+    },
+    
+}
+</script>
