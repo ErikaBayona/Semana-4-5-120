ddbecfaf48e3761d2c87b851b0734c53096cd80b
Yoyler Cordóba <yoilerlp595@gmail.com>
2020-12-21 19:44:44 -0500

    fixing slider

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,9 +2,9 @@
   <div>
     
      <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark ">
-        <a class="navbar-brand " href="# ">
+        <a class="navbar-brand " href="/">
             <img src="https://image.freepik.com/free-vector/established-international-trade-routes-selling-goods-overseas-export-control-export-controlled-materials-export-licensing-services-concept-pinkish-coral-bluevector-isolated-illustration_335657-1794.jpg" width="30 " height="30
-    " class="d-inline-block align-top " alt=" " loading="lazy "> Nombre de la empresa
+    " class="d-inline-block align-top rounded " alt=" " loading="lazy "> Transport 120
         </a>
         <div class="collapse navbar-collapse justify-content-end " id="navbarNav ">
             <ul class="navbar-nav ">
@@ -32,7 +32,7 @@
             <li data-target="#carrusel" data-slide-to="2" class=""></li>
         </ol>
         <div class="carousel-inner">
-            <div class="carousel-item active">
+            <div class="carousel-item ">
                 <img src="https://image.freepik.com/free-photo/industrial-port-container-yard_1112-1202.jpg" width="1410" height="500"/>
                 
                 
@@ -41,7 +41,7 @@
                     
                 </div>
             </div>
-            <div class="carousel-item">
+            <div class="carousel-item active">
                 <img src="https://image.freepik.com/free-photo/transportation-logistics-container-cargo-ship-cargo-plane_37416-150.jpg" width="1410" height="500"/>
                 
                 
@@ -62,7 +62,7 @@
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#carrusel" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
@@ -72,10 +72,10 @@
 
     <div class="card text-white bg-dark mb-3 no-margin-bnottom" >
   <div class="row g-0">
-    <div class="col-md-4">
-      <img src="https://image.freepik.com/free-vector/established-international-trade-routes-selling-goods-overseas-export-control-export-controlled-materials-export-licensing-services-concept-pinkish-coral-bluevector-isolated-illustration_335657-1794.jpg" >
+    <div class="col-md-5">
+      <img  class="rounded"  src="https://image.freepik.com/free-vector/established-international-trade-routes-selling-goods-overseas-export-control-export-controlled-materials-export-licensing-services-concept-pinkish-coral-bluevector-isolated-illustration_335657-1794.jpg" >
     </div>
-    <div class="col-md-8" style="padding: 30px 0px 0px 160px;">
+    <div class="col-md-7" style="padding: 30px 0px 0px 160px;">
       <div class="card-body">
         <h1 class="card-title text-center">¿QUIENES SOMOS?</h1>
         <h4 class="card-text text-center">Somos un operador logistico especializado y con mayor cobertura, garantizamos integridad en toda su mercancia y contamos con una ámplia y extensa capacidad de almacenamiento en diversas locaciones estrátegicas en el país, en búsqueda de otorgar la posibilidad de crear puntos de distribución operativamente viables para nuestros cliente.
@@ -154,7 +154,7 @@
         <div v-for="miembro in team" :key="miembro.codigo" class="col mt-5" style="margin-top:1rem!important">
           <div class="card text-white bg-dark">
             <div class="d-flex justify-content-center p-2">
-              <img :src="miembro.image" />
+              <img :src="miembro.image" width="100px" height="132px" />
             </div>
             <div class="card-body">
               <h5 class="card-title ">
@@ -230,6 +230,7 @@
 </template>
 
 <script>
+
 export default {
   data() {
     return {
